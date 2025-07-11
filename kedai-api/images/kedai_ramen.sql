1ccac507a451447912c65808a77d2549817188a0
Reinal <161219042+renal07@users.noreply.github.com>
2025-07-12 05:42:43 +0700

    Add files via upload

--- kedai-api/images/kedai_ramen.sql
+++ kedai-api/images/kedai_ramen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Jul 2025 pada 12.53
+-- Waktu pembuatan: 12 Jul 2025 pada 00.41
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -28,9 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `login` (
-  `username` varchar(20) NOT NULL,
-  `password` varchar(10) NOT NULL
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '123', 'admin'),
+(2, 'rajwa', '456', ''),
+(3, 'stikom', '123', 'admin');
 
 -- --------------------------------------------------------
 
@@ -41,7 +52,6 @@ CREATE TABLE `login` (
 CREATE TABLE `produk` (
   `id` int(11) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `deskripsi` text DEFAULT NULL,
   `harga` decimal(10,2) DEFAULT NULL,
   `gambar` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -50,16 +60,21 @@ CREATE TABLE `produk` (
 -- Dumping data untuk tabel `produk`
 --
 
-INSERT INTO `produk` (`id`, `nama`, `deskripsi`, `harga`, `gambar`) VALUES
-(1, 'Yokohama Ramen', 'Ramen khas Yokohama', 20000.00, 'Ramen1.jpg'),
-(2, 'Nakamoto Ramen', 'Ramen khas Nakamoto', 25000.00, 'Ramen2.jpg'),
-(3, 'Shibuya Ramen', 'Ramen khas Shibuya', 30000.00, 'Ramen3.jpg'),
-(9, 'Shizu Ramen', 'Ramen khas otentik daerah jepang', 12000.00, ''),
-(10, 'mie', 'aaa', 10000.00, 'Ramen2.jpg');
+INSERT INTO `produk` (`id`, `nama`, `harga`, `gambar`) VALUES
+(2, 'Ramen Ayam', 16000.00, 'Ramen2.jpg'),
+(5, 'Ramen Gurih', 28000.00, 'Ramen1.jpg'),
+(6, 'Ramen Ikan', 30000.00, 'Ramen3.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indeks untuk tabel `produk`
@@ -72,10 +87,16 @@ ALTER TABLE `produk`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
