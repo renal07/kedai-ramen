85241680166ba263c17a4fcbe2febaba4db309f2
rajwa123321 <rajwaaanggara123@gmail.com>
2025-07-12 05:53:27 +0700

    css beli

--- src/pages/Beli.jsx
+++ src/pages/Beli.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import './About.css';
 
 function Beli() {
   return (
     <div>
       <Header />
       <Navbar />
-      <main className="beli-main">
+      <main className="about-main">
         <h2>Halaman Beli</h2>
         <p>Silakan kunjungi kedai kami untuk melakukan pembelian langsung.</p>
       </main>
